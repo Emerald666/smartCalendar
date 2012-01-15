539b1c4ae9135a45ea2674b92a399efc43657c22
Emerald666 <aminesahibi@gmail.com>
2012-01-14 21:53:30 -0500

    almost done with the event view

--- application/views/users_area.php
+++ application/views/users_area.php
@@ -24,22 +24,30 @@
                 });
                 $('.time').datetimepicker({
                 onClose: function(dateText, inst){
-                    if(dateText!=""){
-                        var date=new Date(dateText);
-                        //alert(date.getTime());
-                        //alert(date.getDay()+"/"+date.getMonth()+"/"+date.getYear()+" "+date.getHours()+":"+date.getMinutes());
+                        if(dateText!=""){
+                            var date=new Date(dateText);
+                            //alert(date.getTime());
+                            //alert(date.getDay()+"/"+date.getMonth()+"/"+date.getYear()+" "+date.getHours()+":"+date.getMinutes());
+                        }
                     }
-                }
-            });
+                });
+               $('.title').click(function(){
+                     $(this).closest('.event').find('.content').slideToggle(500);
+                });
              });
         </script>
        <style type="text/css">
-           body{
+          body{
                background: transparent url(<?php echo base_url("/application/libraries/images/noise.png");?>) repeat 0 0;
+               margin-top:50px;
             }
-           h1{
+          h1{
                font-family: Shadows Into Light, Arial, Helvetica, sans-serif;
                font-size: 68px;
+           }
+          h2{
+               font-family: Shadows Into Light, Arial, Helvetica, sans-serif;
+               font-size: 48px;
            }
            #container{
                margin-top: 40px;
@@ -98,6 +106,42 @@
         #addContainer, #eventForm{
             margin-bottom:25px;
         }
+        
+        .styleContainer{
+            color: white !important;
+            border: 1px solid #2F5BB7 !important;
+            background: #3F83F1;
+            background: -webkit-linear-gradient(top, #4D90FE, #357AE8);
+            background: -moz-linear-gradient(top, #4D90FE, #357AE8);
+            background: -ms-linear-gradient(top, #4D90FE, #357AE8);
+            border-bottom-style:dotted;
+            border-width:1px;
+            height:20px;
+            margin-top:2px;
+            text-align:center;
+        }
+
+        .eventContainer{
+            border-bottom-style:dotted;
+            border-width:1px;
+        }
+
+        .eventContainer:hover{
+            cursor:pointer
+        }
+
+        .event{
+            margin-bottom:20px;
+        }
+
+        .title{
+            margin-left:5px;
+            margin-top:5px;
+        }
+        .content{
+            display:none;
+        }
+
 
        </style>
     </head>
@@ -132,8 +176,51 @@
                     echo validation_errors();
                 ?>
             </div>
-            <div id="event" class="grid_16">
-                <p>Hello World s;fmlewmcdsaclkmdsclkdsamclkdsmckdsamclkmdslkcmslkmcdsmclksmcmsa;lk</p>
+            <div id="EventsHeader" class="grid_16 push_6">
+                <h2>Events!</h2>
+            </div>
+            <div class="event grid_16">
+                <div class="styleContainer grid_2 alpha">
+                     <span>12:00-14:00</span>
+                </div>
+                <div class="eventContainer grid_13 alpha">
+                    <p class="title">Fall Bird Walks</p>
+                    <div class="content">
+                        <ul>
+                            <li><b>Host: </b>Jason Derouloux</li>
+                            <li><b>Where: </b>Wherever you want</li>
+                            <li><b>When: </b>12:00-14:00</li>
+                            <li><b>Description: </b> can accomplish this, but it's all or nothing.
+                                I click one minimize button, and everything goes. How can I contain
+                                it to just toggle items with a certain class name that are children of
+                                the div that the minimize button is in? I've tried .parent(), .child(), .next(),
+                                .prev(). I've thrown everything at it that I know. Now I turn to you, kind sirs (or madams).
+                                What am I doing wrong? Here's the ul I'm trying to run it on.</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="commandContainer grid_1 alpha">
+                    <a href="#" class="button on" title="Edit event"><span class="icon icon145"></span></a>
+                </div>
+            </div>
+             <div class="event grid_16">
+                <div class="styleContainer grid_2 alpha">
+                    <span>12:00-16:00</span>
+                </div>
+                <div class="eventContainer grid_13 alpha">
+                    <p class="title">Cast in Bronze: A Workshop in Exploring and Creating Bronze Sculpture</p>
+                    <div class="content">
+                        <ul>
+                            <li><b>Host:</b></li>
+                            <li><b>Where:</b></li>
+                            <li><b>When:</b></li>
+                            <li><b>Description:</b></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="commandContainer grid_1 alpha">
+                     <a href="#" class="button on" title="Edit event"><span class="icon icon145"></span></a>
+                </div>
             </div>
         </div>
     </body>
